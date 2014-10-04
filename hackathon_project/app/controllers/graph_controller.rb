9559a8fb12001c3dc96753a69acd4da3d4355789
Nathan Seese <seese.28@osu.edu>
2014-10-04 06:21:09 -0400

    further work on graphs

--- hackathon_project/app/controllers/graph_controller.rb
+++ hackathon_project/app/controllers/graph_controller.rb
@@ -1,9 +1,11 @@
 require 'chartkick'
 require 'github_commit'
 
+LogarithmicBreakpoint = 1000
+
 class GraphController < ApplicationController
   def line_graph
-    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])[0, 15]
+    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])
     @additions = @cs.map{|c| c.additions}
     @deletions = @cs.map{|c| c.deletions}
     @dates  = @cs.map{|c| c.date}
