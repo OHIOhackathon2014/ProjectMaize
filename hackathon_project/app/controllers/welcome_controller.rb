d1e59e2d2e521bae40200edbdc50a401564c7740
Olivia Whitman <whitman.68@osu.edu>
2014-10-04 09:11:21 -0400

    merge and beginning to combine graphs and website

--- hackathon_project/app/controllers/welcome_controller.rb
+++ hackathon_project/app/controllers/welcome_controller.rb
@@ -1,5 +1,25 @@
+require 'chartkick'
+require 'github_commit'
+
+LogarithmicBreakpoint = 1000
+
 class WelcomeController < ApplicationController
   def index
-    render js: "javascript file location here"
+    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])
+  end
+
+  def line_graph
+    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])
+    @additions = @cs.map{|c| c.additions}
+    @deletions = @cs.map{|c| c.deletions}
+    @dates  = @cs.map{|c| c.date}
+  end
+
+  def compare
+    @repo_owners_names = params[:repos].split(';')
+    @repos = @repo_owners_names.map{|s| s.split(':')}.map{|r| GithubCommit.generate_commits(r[0], r[1])}
   end
 end
+
+#'OHIOhackathon2014', 'ProjectMaize'
+
