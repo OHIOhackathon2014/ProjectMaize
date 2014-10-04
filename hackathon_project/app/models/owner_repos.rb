5bedcaad4543d9e81b26b9ba49e4648a15e56247
Nathan Seese <seese.28@osu.edu>
2014-10-04 16:55:50 -0400

    added owner repos class

---
+++ hackathon_project/app/models/owner_repos.rb
@@ -0,0 +1,13 @@
+require 'github_commit'
+require 'open-uri'
+require 'json'
+
+class OwnerRepos
+  def self.generate_owner_repo_names(owner)
+    JSON.parse(open("https://api.github.com/users/#{owner}/repos").read).map{|r| r['name']}
+  end
+
+  def self.generate_owner_repo_commits(owner)
+    self.generate_owner_repo_names(owner).map{|name| GithubCommit.generate_commits(owner, name)}
+  end
+end
