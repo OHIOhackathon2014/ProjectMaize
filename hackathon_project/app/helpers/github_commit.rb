ffa3e8bf62cbb124ce12d7acbc0c77505a8549b1
Nathan Seese <seese.28@osu.edu>
2014-10-04 03:46:11 -0400

    changed date representation to DateTime object instead of string

--- hackathon_project/app/helpers/github_commit.rb
+++ hackathon_project/app/helpers/github_commit.rb
@@ -4,11 +4,11 @@ require 'json'
 Token = 'ae8911808ca7162e61ab46bdc79bd7d2e3fe41b5' # this probably shouldn't be hardcoded, change this later
 
 class GithubCommit
-  attr_accessor :date
   attr_accessor :name
   attr_accessor :url
   attr_accessor :json
-  
+  attr_accessor :date
+
   def generate_json!
     @json = JSON.parse(open(url).read)
   end
@@ -25,13 +25,17 @@ class GithubCommit
     @json['stats']['total']
   end
   
+  def date_parse(d)
+    @date = DateTime.parse d
+  end
+  
   # Example: generate_commits('OHIOhackathon2014', 'ProjectMaize')
   def self.generate_commits(owner, repo_name)
     url = "https://api.github.com/repos/#{owner}/#{repo_name}/commits?access_token=#{Token}"
     parsed_json = JSON.parse open(url).read
     return parsed_json.map do |j|
       c = self.new
-      c.date = j['commit']['committer']['date']
+      c.date_parse j['commit']['committer']['date']
       c.name = j['commit']['committer']['name']
       c.url  = j['url'] + "?access_token=#{Token}"
       c.generate_json!
