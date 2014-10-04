6ee803dd8580fcaa6784b0797d481c8072d70ae6
Nathan Seese <seese.28@osu.edu>
2014-10-04 05:32:19 -0400

    switched from google charts to highcharts

--- hackathon_project/app/controllers/graph_controller.rb
+++ hackathon_project/app/controllers/graph_controller.rb
@@ -3,7 +3,7 @@ require 'github_commit'
 
 class GraphController < ApplicationController
   def line_graph
-    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])[0, 10]
+    @cs = GithubCommit.generate_commits(params[:owner], params[:repo_name])[0, 15]
     @additions = @cs.map{|c| c.additions}
     @deletions = @cs.map{|c| c.deletions}
     @dates  = @cs.map{|c| c.date}
